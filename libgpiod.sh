60566fd28dfe9828fc56468845a0bc3bad3baaca
Vladyslav Aviedov <vladaviedov@protonmail.com>
2024-03-01 19:47:48 -0500

    fixed libgpiod script; seems to work?

--- libgpiod.sh
+++ libgpiod.sh
@@ -4,8 +4,8 @@ THREAD_OPT=$1
 cd lib/libgpiod
 mkdir build
 cd build
-PATH=$PATH:$BASE/build/gcc/bin:$BASE/build/gcc/arm-linux-gnueabihf/bin \
-	../autogen.sh \
+export PATH=$PATH:$BASE/build/gcc/bin:$BASE/build/gcc/arm-linux-gnueabihf/bin
+../autogen.sh \
 	--prefix=$BASE/build \
 	--host=arm-linux-gnueabihf \
 	--target=arm-linux-gnueabihf \
