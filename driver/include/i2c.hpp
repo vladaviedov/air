8039af8224eaa88fd14bdf0ff86576a870a15606
Scott Abramson <scottrabramson@gmail.com>
2024-03-19 10:59:45 -0400

    fixxed silly mistakes

--- driver/include/i2c.hpp
+++ driver/include/i2c.hpp
@@ -4,6 +4,8 @@
  */
 #pragma once
 
+#include <cstdint>
+
 class i2c {
 
 public:
@@ -11,18 +13,18 @@ public:
     /**
      * @brief Constructor. Initializes I2C device.
      *
-     * @param[in] dev_addr - address of the I2C slave device.
+     * @param[in] addr - device address of the I2C slave device.
      */
-    i2c(const uint8_t dev_addr);
+    i2c(const uint8_t addr, const int adapter_nr);
 
     /** 
      * @brief I2C read.
      *
-     * @param[in] data - Data buffer to write to.
+     * @param[out] data - Data buffer to write to.
      * @param[in] size - Size of data buffer.
-     * @return error code
+     * @return number of bytes read
      */
-    int read(char* data, uint8_t size);
+    const int read(char* data, const uint32_t size);
 
     /** 
      * @brief I2C write.
@@ -30,12 +32,11 @@ public:
      * @param[in] reg - Register to read from.
      * @param[in] data - Data buffer to write.
      * @param[in] size - Size of data buffer.
-     * @return error code
+     * @return number of bytes read
      */
-    int write(uint8_t reg, char* data, uint8_t size);
+    const boolean write(const char* data, const uint32_t size);
 
 private:
     int fd;
     const uint8_t dev_addr;
-    
 }
--- driver/src/i2c.cpp
+++ driver/src/i2c.cpp
@@ -7,36 +7,27 @@
 #include <linux/i2c-dev.h>
 #include <i2c/smbus.h>
 
-i2c::i2c(uint8_t dev_addr) {
-    int fd;
-    int adapter_nr = 1;
+i2c::i2c(const uint8_t addr, const int adapter_nr) {
+
+    dev_addr = addr;
+
     char filename[32];
 
-    snprintf(filename, 31, "/dev/i2c-%d", adapter_nr);
+    snprintf(filename, 32, "/dev/i2c-%d", adapter_nr);
     fd = open(filename, O_RDWR);
     if (fd < 0) {
         throw std::runtime_error("Failed to open i2c port");
     }
 
-    if (ioctl(fd, I2C_SLAVE, addr) < 0) {
+    if (ioctl(fd, I2C_SLAVE, dev_addr) < 0) {
         throw std::runtime_error("Failed to open i2c port (ioctl)");
     }
 }
 
-i2c::read(char* data, uint8_t size) {
-    size += 1;
-    if (read(fd, data, size) != size) {
-        throw std::runtime_error("Failed to read i2c");
-    }
+int i2c::read(char* data, const uint32_t size) {
+    return read(fd, data, size);
 }
 
-i2c::write(uint8_t reg, char* data, uint8_t size) {
-    char* buf;
-    *buf[0] = reg;
-    std::copy(data, data + size, buf + 1);
-    // size is now the size of buf
-    size += 1;
-    if (write(fd, data, size) != size) {
-        throw std::runtime_error("Failed to write i2c");
-    }
+boolean i2c::write(const char* data, const uint32_t size) {
+    return write(fd, data, size) == size;
 }
