09c63d2919edf7f1a9cb136e05136a36ce3d6a46
Scott Abramson <scottrabramson@gmail.com>
2024-03-20 10:13:31 -0400

    i2c fixes

--- driver/include/i2c.hpp
+++ driver/include/i2c.hpp
@@ -7,38 +7,37 @@
 #include <cstdint>
 
 class i2c {
-
 public:
+	/**
+	 * @brief Constructor. Initializes I2C device.
+	 *
+	 * @param[in] addr - device address of the I2C slave device.
+	 */
+	i2c(uint8_t addr, int adapter_nr);
 
-    /**
-     * @brief Constructor. Initializes I2C device.
-     *
-     * @param[in] addr - device address of the I2C slave device.
-     */
-    i2c(const uint8_t addr, const int adapter_nr);
+	~i2c();
 
-    ~i2c();
+	/**
+	 * @brief Read up to 32 bytes.
+	 *
+	 * @param[in] reg - Register to read from.
+	 * @param[out] data - Data buffer to write to.
+	 * @param[in] size - Size of data buffer.
+	 * @return number of bytes read.
+	 */
+	int read(uint8_t reg, uint8_t *data) const;
 
-    /** 
-     * @brief I2C read.
-     *
-     * @param[out] data - Data buffer to write to.
-     * @param[in] size - Size of data buffer.
-     * @return number of bytes read
-     */
-    const int read(char* data, const uint32_t size);
-
-    /** 
-     * @brief I2C write.
-     *
-     * @param[in] reg - Register to read from.
-     * @param[in] data - Data buffer to write.
-     * @param[in] size - Size of data buffer.
-     * @return number of bytes read
-     */
-    const boolean write(const char* data, const uint32_t size);
+	/**
+	 * @brief Write up to 32 bytes.
+	 *
+	 * @param[in] reg - Register to write to.
+	 * @param[in] data - Data buffer to write.
+	 * @param[in] size - Size of data buffer.
+	 * @return Success (true) or failure (not true).
+	 */
+	bool write(uint8_t reg, const uint8_t *data, uint8_t size) const;
 
 private:
-    int fd;
-    const uint8_t dev_addr;
-}
+	int fd;
+	uint8_t dev_addr;
+};
--- driver/src/i2c.cpp
+++ driver/src/i2c.cpp
@@ -4,34 +4,38 @@
  */
 #include "i2c.hpp"
 
+#include <cstdio>
+#include <fcntl.h>
 #include <linux/i2c-dev.h>
+#include <stdexcept>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
 #include <i2c/smbus.h>
 
-i2c::i2c(const uint8_t addr, const int adapter_nr) {
+i2c::i2c(uint8_t addr, int adapter_nr)
+	: dev_addr(addr) {
+	char filename[32];
 
-    dev_addr = addr;
+	snprintf(filename, 32, "/dev/i2c-%d", adapter_nr);
+	fd = open(filename, O_RDWR);
+	if (fd < 0) {
+		throw std::runtime_error("Failed to open i2c port");
+	}
 
-    char filename[32];
-
-    snprintf(filename, 32, "/dev/i2c-%d", adapter_nr);
-    fd = open(filename, O_RDWR);
-    if (fd < 0) {
-        throw std::runtime_error("Failed to open i2c port");
-    }
-
-    if (ioctl(fd, I2C_SLAVE, dev_addr) < 0) {
-        throw std::runtime_error("Failed to open i2c port (ioctl)");
-    }
+	if (ioctl(fd, I2C_SLAVE, dev_addr) < 0) {
+		throw std::runtime_error("Failed to open i2c port (ioctl)");
+	}
 }
 
 i2c::~i2c() {
-    fd.close();
+	close(fd);
 }
 
-int i2c::read(char* data, const uint32_t size) {
-    return read(fd, data, size);
+int i2c::read(uint8_t reg, uint8_t *data) const {
+	return i2c_smbus_read_block_data(fd, reg, data);
 }
 
-boolean i2c::write(const char* data, const uint32_t size) {
-    return write(fd, data, size) == size;
+bool i2c::write(uint8_t reg, const uint8_t *data, uint8_t size) const {
+	return i2c_smbus_write_block_data(fd, reg, size, data) == 0;
 }
