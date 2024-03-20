3a46982def2e7524e16e5fe0b2f27e27617f247e
Caio <caiodasilva05@outlook.com>
2024-03-20 18:05:57 -0400

    new branch whoopsies

--- control/src/main.cpp
+++ control/src/main.cpp
@@ -3,7 +3,5 @@
 #include <shared/temp.hpp>
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
-	placeholder();
-	return 0;
+	
 }
---
+++ shared/include/messages.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+struct msg_t {
+    std::string caller_id;
+    std::string receiver_id;
+    std::string body;
+};
+
+
+std::string format_message(const msg_t &msg);
+msg_t parse_message(const std::string &str_msg);
