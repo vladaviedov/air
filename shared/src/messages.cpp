f06bdc5d526e2999c966b1f1b80c4d2e0b2e01f7
Caio <caiodasilva05@outlook.com>
2024-03-27 12:36:50 -0400

    ran linter

--- shared/src/messages.cpp
+++ shared/src/messages.cpp
@@ -13,28 +13,25 @@ constexpr std::string MSG_HEADER = "AIRv1.0";
 constexpr std::string UNSUPPORTED = "UN";
 
 bool validate_header(const std::string &str) {
-	if (str == MSG_HEADER) 
+	if (str == MSG_HEADER)
 		return true;
 	return false;
 }
 
 bool validateId(const std::string &str) {
-
 	if (str.length() < 2) {
 		return false;
 	}
 
-	if (str.substr(0,2) == UNSUPPORTED) {
+	if (str.substr(0, 2) == UNSUPPORTED) {
 		return false;
 	}
 
 	for (size_t i = 0; i < str.length(); i++) {
 		char c = str[i];
-		if (!isalnum(c) && c != '/' && c != '-') 
-			return false;	
+		if (!isalnum(c) && c != '/' && c != '-')
+			return false;
 	}
 
 	return true;
 }
-
-
