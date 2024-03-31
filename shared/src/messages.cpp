eeb232f1ceb39a3499cc9707bebd45e3a633f769
Caio <caiodasilva05@outlook.com>
2024-03-31 19:17:09 -0400

    added reading from file

--- shared/src/messages.cpp
+++ shared/src/messages.cpp
@@ -15,7 +15,7 @@ static constexpr std::string MSG_HEADER = "AIRv1.0";
 static constexpr std::string UNSUPPORTED = "UN";
 static constexpr std::string ID_FILE = "/etc/airid";
 
-// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables) 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 static std::shared_ptr<std::string> control_id = nullptr;
 
 bool validate_header(const std::string &str) {
@@ -42,7 +42,7 @@ bool validate_id(const std::string &str) {
 	return str.length() < 2 || str.substr(0, 2) != UNSUPPORTED;
 }
 
-const std::shared_ptr<std::string> &read_id() {
+const std::shared_ptr<std::string> &get_id() {
 	if (control_id == nullptr) {
 		control_id = std::make_shared<std::string>();
 
