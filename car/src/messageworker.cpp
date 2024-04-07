6bcb63500b5893c49d12c6548e9840ef11a5334f
Caio <caiodasilva05@outlook.com>
2024-04-07 14:42:11 -0400

    added comments and fixed issues

--- car/src/messageworker.cpp
+++ car/src/messageworker.cpp
@@ -1,3 +1,7 @@
+/**
+ * @file src/messageworker.cpp
+ * @brief Message handler for car
+*/
 #include "messageworker.hpp"
 
 #include <sstream>
@@ -9,19 +13,19 @@ static constexpr std::string MSG_HEADER = "AIRv1.0";
 static constexpr std::string CHECK = "CHK";
 static constexpr std::string ACKNOWLEDGE = "ACK";
 
+#define MESSAGE_TIMEOUT 100 /*time to wait for message (in frames)*/
+
 message_worker::message_worker(const std::shared_ptr<tdma> &tdma_handler_in)
 	: tdma_handler(tdma_handler_in) {
 	car_id = get_id();
 }
 
 void message_worker::await_checkin() {
-	tdma_handler->tx_sync(format_checkin());
+	tdma_handler->tx_sync(format_checkin()); // send check in 
 
-	std::string rx_msg;
-	while (rx_msg.empty()) {
-		rx_msg = tdma_handler->rx_sync(UINT32_MAX);
-	}
+	std::string rx_msg = tdma_handler->rx_sync(MESSAGE_TIMEOUT); // receive check in
 
+	/*
 	std::istringstream parts(rx_msg);
 	std::string header;
 	std::string check;
@@ -34,6 +38,7 @@ void message_worker::await_checkin() {
 	if (!parts.eof() || check != CHECK) {
 		throw std::invalid_argument("Check is invalid");
 	}
+	*/
 }
 
 std::string message_worker::format_checkin() {
@@ -41,7 +46,11 @@ std::string message_worker::format_checkin() {
 }
 
 std::string message_worker::format_request(uint8_t desired_pos) {
-	return *car_id + " " + std::to_string(current_pos | desired_pos);
+	uint8_t request_data = current_pos | desired_pos;
+	std::string formatted_request;
+	formatted_request.append(*car_id + " ");
+	formatted_request.push_back(request_data);
+	return formatted_request;
 }
 
 std::string message_worker::format_acknowledge(std::string command) {
--- car/src/messageworker.hpp
+++ car/src/messageworker.hpp
@@ -1,7 +1,11 @@
+/**
+ * @file src/messageworker.hpp
+ * @brief Message handler for car
+*/
 #pragma once
 
 #include <memory>
-#include <stdlib.h>
+#include <cstdlib>
 #include <string>
 
 #include <shared/tdma.hpp>
@@ -9,10 +13,24 @@
 class message_worker {
 public:
 	message_worker(const std::shared_ptr<tdma> &tdma_handler_in);
+	/**
+	 * @brief send and receive check in from control
+	*/
 	void await_checkin();
 
+	/**
+	 * @brief create check in message
+	*/
 	std::string format_checkin();
+	/**
+	 * @param desired_pos position car is requesting to go
+	 * @brief creates request for car with id, current position, and desired position
+	*/
 	std::string format_request(uint8_t desired_pos);
+	/**
+	 * @param command given by control
+	 * @brief acknowledges command given by control
+	*/
 	std::string format_acknowledge(std::string command);
 
 private:
