95bd555c4e1998b80328e39fea5bc3a766fd8174
Caio <caiodasilva05@outlook.com>
2024-04-09 21:19:09 -0400

    finished message worker

---
+++ car/src/messageworker.cpp
@@ -0,0 +1,100 @@
+/**
+ * @file src/messageworker.cpp
+ * @brief Message handler for car
+ */
+#include "messageworker.hpp"
+
+#include <sstream>
+#include <stdexcept>
+#include <functional>
+
+#include <shared/messages.hpp>
+
+static constexpr std::string MSG_HEADER = "AIRv1.0";
+static constexpr std::string CHECK = "CHK";
+static constexpr std::string ACKNOWLEDGE = "ACK";
+static constexpr std::string STANDBY = "SBY";
+static constexpr std::string GO_REQUESTED = "GRQ";
+static constexpr std::string CLEAR = "CLR";
+static constexpr std::string FINAL = "FIN";
+
+#define MESSAGE_TIMEOUT 4 /*time to wait for message (in frames)*/
+
+message_worker::message_worker(const std::shared_ptr<tdma> &tdma_handler_in)
+	: tdma_handler(tdma_handler_in) {
+	car_id = get_id();
+}
+
+std::string message_worker::await_checkin() {
+	tdma_handler->tx_sync(format_checkin()); // send check in
+
+	std::string control_id;
+
+	while (true) {
+
+		control_id = tdma_handler->rx_sync(MESSAGE_TIMEOUT); // receive check in
+
+		if (!validate_id(control_id)) {
+			continue;
+		}
+		
+		break;
+	}
+
+	uint8_t desired_pos = 0; //choose random pos
+	send_request(desired_pos);
+
+	return control_id;
+}
+
+message_worker::command message_worker::send_request(uint8_t desired_pos) {
+	
+	tdma_handler->tx_sync(format_request(desired_pos));
+	std::string command;
+	uint32_t i = 0;
+
+	while (i < 3) {
+		i++;
+
+		std::string response =
+			tdma_handler->rx_sync(MESSAGE_TIMEOUT); // receive check in
+
+		std::istringstream parts(response);
+		std::string ack;
+
+		parts >> ack;
+		if (parts.eof() || ack != ACKNOWLEDGE) {
+			continue;
+		}
+		parts >> command;
+		if (!parts.eof()) {
+			continue;
+		}
+	}
+
+	tdma_handler->tx_sync(ACKNOWLEDGE);
+	
+	if (command == STANDBY) {
+		return SBY;
+	}
+	else if (command == GO_REQUESTED) {
+		return GRQ;
+	}
+	else {
+		throw std::invalid_argument("Unsupported");
+	}
+}
+
+std::string message_worker::format_checkin() {
+	return MSG_HEADER + " " + CHECK;
+}
+
+std::string message_worker::format_request(uint8_t desired_pos) {
+	std::string formatted_request;
+	formatted_request.append(*car_id + " ");
+	formatted_request.push_back(current_pos);
+	formatted_request.push_back(desired_pos);
+	return formatted_request;
+}
+
+
---
+++ car/src/messageworker.hpp
@@ -0,0 +1,55 @@
+/**
+ * @file src/messageworker.hpp
+ * @brief Message handler for car
+ */
+#pragma once
+
+#include <cstdlib>
+#include <memory>
+#include <string>
+#include <functional>
+
+#include <shared/tdma.hpp>
+
+class message_worker {
+public:
+	enum command {
+		SBY,
+		GRQ,
+	};
+	/**
+	 * @brief constructor for car message worker
+	 * @param[in] tdma_handler_in
+	 */
+	message_worker(const std::shared_ptr<tdma> &tdma_handler_in);
+
+	/**
+	 * @brief send and receive check in from control
+	 * @return control id
+	 */
+	std::string await_checkin();
+
+	/**
+	 * @brief send request callback
+	*/
+	message_worker::command send_request(uint8_t desired_pos);
+
+	/**
+	 * @brief create check in message
+	 * @return check in message
+	 */
+	std::string format_checkin();
+
+	/**
+	 * @param[in] desired_pos position car is requesting to go
+	 * @brief creates request for car with id, current position, and desired
+	 * position
+	 * @return request message
+	 */
+	std::string format_request(uint8_t desired_pos);
+
+private:
+	std::shared_ptr<tdma> tdma_handler;
+	std::shared_ptr<std::string> car_id;
+	uint8_t current_pos;
+};
